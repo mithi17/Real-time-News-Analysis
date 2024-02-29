9e6d2cc23ad9c1cd4fddcaa7e1e87f550828721d
mithi17 <mithileshkumar170203@gmail.com>
2024-03-01 04:51:16 +0530

    updated

--- application/routes.py
+++ application/routes.py
@@ -2,6 +2,8 @@ from application import app,render_template,request, redirect, url_for,db,dateti
 from apscheduler.schedulers.background import BackgroundScheduler
 from urllib.parse import urlparse
 from bson.objectid import ObjectId
+from flask import jsonify
+
    
 scheduler = BackgroundScheduler()
 
@@ -12,9 +14,18 @@ def index():
     if request.method == 'POST':
         url = request.form['url']
         now = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-        db.urls.insert_many([{'url': url, 'date_time': now }])
+        db.home_url.insert_many([{'url': url, 'date_time': now }])
         return redirect(url_for('matches')) 
     return render_template('index.html')
+
+# @app.route('/update_table', methods=['POST'])
+# def update_table():
+#     data = request.get_json()
+#     row_index = data['rowIndex']
+#     column_index = data['columnIndex']
+#     value = data['value']
+
+#     return jsonify({'Updated': 'success'})
 
 @app.route("/keys", methods=["GET","POST"])
 def keys():
@@ -26,21 +37,29 @@ def keys():
         return redirect(url_for('matches')) 
     return render_template('index.html')
 
-@app.route("/matches", methods=['GET','POST'])
+@app.route("/info", methods=['GET','POST'])
 def matches():
+    cursor = db.main_url.find()
+    main_urls = list(cursor)
     cursor = db.key.find()
     match_data = list(cursor)
-    cursor = db.urls.find()
+    cursor = db.home_url.find()
     url_s = list(cursor)
-    info_cursor = db.information.find().sort('_id', 1).limit(1)
+    info_cursor = db.information.find().sort('_id', -1).limit(1)
     latest_info = list(info_cursor)[0]
-    return render_template('index.html', match_data=match_data, latest_info=latest_info, url_s=url_s)
+    return render_template('info.html', match_data=match_data, latest_info=latest_info, url_s=url_s, main_urls=main_urls)
 
     # return render_template('index.html', match_data=match_data)
 
+@app.route("/main_url", methods=['GET','POST'])
+def sub_urls():
+    cursor = db.main_url.find()
+    main_urls = list(cursor)
+    return render_template('main_url.html', main_urls=main_urls)
+
 @app.route("/stored", methods=['GET','POST'])
-def stored():
-    cursor = db.urls.find()
+def main_urls():
+    cursor = db.home_url.find()
     url_data = list(cursor)
     for url_doc in url_data:
         url = url_doc['url']
@@ -48,77 +67,90 @@ def stored():
             url = "https://" + url
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
-        title = soup.title.string
-        links = []
         for link in soup.find_all('a'):
             href = link.get('href')
             if href and re.match('^https?://', href):
-                links.append(href)
-        # Check if the URL already exists in the url_links collection
-        url_exists = db.url_links.find_one({'url': url}, {'_id': 1})
-        if url_exists:
-            # If the URL exists, update the existing document
-            db.url_links.update_one({'url': url}, {'$set': {'links': links, 'date_time': url_doc['date_time']}})
-        else:
-            # If the URL does not exist, insert a new document
-            db.url_links.insert_one({'url': url,'links': links, 'date_time': url_doc['date_time']})
-    get_information()
-    return redirect(url_for('matches'))
+                # Check if the url already exists in the main_url collection
+                if db.main_url.find_one({'url': href}):
+                    continue                  # Skip the current iteration if the url already exists
+                # Save each link as a separate document in the main_url collection
+                db.main_url.insert_one({'url': href, 'date_time': url_doc['date_time']})
+    sub_url_collection()
+    return redirect(url_for('matches')) 
 
+           
+def sub_url_collection():
+    cursor = db.main_url.find()
+    main_url_data = list(cursor)
+    for main_url_doc in main_url_data:
+        response = requests.get(main_url_doc['url'])
+        soup = BeautifulSoup(response.content, 'html.parser')
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            if href and re.match('^https?://', href):
+                # Check if the url already exists in the sub_url collection
+                if db.sub_urls.find_one({'url': href}):
+                    continue  # Skip the current iteration if the url already exists
+                # Save each link as a separate document in the sub_url collection
+                db.sub_urls.insert_one({'url': href, 'date_time': main_url_doc['date_time']})
+    # return redirect(url_for('matches'))
+
+
+@app.route("/informations", methods=['GET','POST'])
 def get_information():
-    print("checking completed")
-    cursor = db.key.find()
-    keywords = list(cursor)
-    cursor = db.url_links.find()
+    cursor = db.sub_urls.find()
     url_links = list(cursor)
     for url_link in url_links:
         url = url_link['url']
-        links = url_link['links']
-        for link in links:
-            if any(keyword_text in link for keyword_text in (keyword['keyword'] for keyword in keywords)):
-                if not db.information.find_one({'url': link}):
-                    response = requests.get(link)
-                    soup = BeautifulSoup(response.content, 'html.parser')
+        if not db.information.find_one({'url': url}):
+            response = requests.get(url_link['url'])
+            # print(url_link)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            # print(soup)
 
-                    # author
-                    editor_div = soup.find('div', class_='editor-details-new-change')
-                    if editor_div:
-                        a_tag = editor_div.find('a', class_='bulletProj')
-                        if a_tag:
-                            author_name = a_tag.text.strip()
-                        else:
-                            author_name = "Express Web Desk" if not a_tag else a_tag.text.strip()
+            # author
+            editor_div = soup.find('div', class_='editor-details-new-change')
+            if editor_div:
+                a_tag = editor_div.find('a', class_='bulletProj')
+                if a_tag:
+                    author_name = a_tag.text.strip()
+                else:
+                    author_name = "Express Web Desk" if not a_tag else a_tag.text.strip()
+            else:
+                author_name = ''
+                
+            # paragraph
+            paragraph_tags = soup.find_all('p')
+            if paragraph_tags:
+                paragraph_texts = [tag.get_text(strip=True) for tag in paragraph_tags]
+            else:
+                paragraph_texts = 'No paragraph Found'
+                
+            # date_time
+            date_time_span = soup.find('span', {'itemprop': 'dateModified'})
+            if date_time_span:
+                date_time = date_time_span.get_text().replace('Updated: ', '')
+            else:
+                date_time = 'No date-time Found'
 
-                    # paragraph
-                    paragraph_tags = soup.find_all('p')
-                    paragraph_texts = [tag.get_text(strip=True) for tag in paragraph_tags]
+            # heading
+            heading = soup.title.string if  soup.title else ''
 
-                    # date_time
-                    date_time_span = soup.find('span', {'itemprop': 'dateModified'})
-                    if date_time_span:
-                        date_time = date_time_span.get_text().replace('Updated: ', '')
-                    else:
-                        date_time = ''
+            # path
+            parsed_url = urlparse(url)
+            path = parsed_url.path
 
-                    # heading
-                    heading = soup.title.string
+            information = {
+                'url': url,
+                'path': path,
+                'updated_on': date_time,
+                'author': author_name,
+                'heading': heading if heading else 'No Heading Found!',
+                'paragraph': [paragraph_texts]
+            }
+            db.information.insert_one(information)
+    return redirect(url_for('matches'))
 
-                    # path
-                    parsed_url = urlparse(url)
-                    path = parsed_url.path
-
-                    information = {
-                        'keyword': [keyword_text for keyword_text in (keyword['keyword'] for keyword in keywords) if keyword_text in link],
-                        'url': link,
-                        'path': path,
-                        'updated_on': date_time,
-                        'author': author_name,
-                        'heading': heading,
-                        'paragraph': [paragraph_texts]
-                    }
-                    db.information.insert_one(information)
-
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(get_information, 'interval', hours=3)
-scheduler.start()
+# scheduler = BackgroundScheduler()
+# scheduler.add_job(get_information, 'interval', hours=3)
+# scheduler.start()
